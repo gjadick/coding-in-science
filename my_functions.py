8ff1d7a1041e79e1f24b6fcdf112659306fbfe5d
Gia Jadick <giajadick@gmail.com>
2023-06-12 10:14:44 -0500

    added function ex

---
+++ my_functions.py
@@ -0,0 +1,22 @@
+###
+#
+# An example file to demonstrate packages/modules.
+#
+###
+
+
+def hello(text):
+    '''
+    print a hello message.
+    usage: hello(text)
+    '''
+    print('Hello,', text)
+    
+def goodbye(text):
+    '''
+    print a goodbye message.
+    usage: goodbye(text)
+    '''
+    print('Goodbye,', text)
+    
+    
